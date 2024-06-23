26dc899bf47976744e3af73e5813e0ff2f428114
espoire <a.pergrossi@gmail.com>
2024-06-23 18:24:47 -0400

    Add wind

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,12 +1,14 @@
 import "./Scene.css";
 import Sea from "./Sea.jsx";
 import Sky from "./Sky.jsx";
+import Wind from "./Wind.jsx";
 
 export default function Scene() {
   return (
     <>
       <Sky />
       <Sea layers="11" />
+      <Wind intensity={0.2} />
     </>
   );
 }
